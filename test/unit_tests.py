5252c66be40a6d8a97816e0ccc2472c36c57dfe1
vonHousen <42911623+vonHousen@users.noreply.github.com>
2020-05-16 17:36:29 +0200

    Finished test_checking_connectivity

--- test/unit_tests.py
+++ test/unit_tests.py
@@ -1,15 +1,38 @@
 import unittest
-from igraph import *
-import algorithm as alg
+from algorithm import *
 
 
 class MyTestCase(unittest.TestCase):
 
     def test_checking_connectivity(self):
+        # test as described in documentation
+        # graph depicted on fig. 6 in documentation
+        full_adjacency_matrix_sample = [
+            [-1,-1, 0, 0],  # a
+            [-1, 0,-1, 0],  # b
+            [ 0,-1,-1, 0],  # c
+            [ 0, 0, 0,-1],  # d
+            [ 0, 0, 0,-1]   # e
+        ]
+        labels = ["a", "b", "c", "d", "e"]
+        weights = [1, 1, 1, 1, 1]
+        graph = PartiallyDirectedGraph(full_adjacency_matrix_sample, labels, weights)
+        # graph.plot()
+        self.assertEqual(False, graph.is_connected())
 
-        g = Graph([(0, 1), (0, 2), (2, 1), (3, 4)])
-        g.vs["name"] = ["a", "b", "c", "d", "e"]
-        self.assertEqual(False, alg.is_connected(g))
+    def test_transforming_g_to_g1(self):
+        # graph depicted on fig. 2 in documentation
+        full_adjacency_matrix_sample = [
+            [ 1, 0,-1],  # a
+            [-1,-1, 0],  # b
+            [ 0,-1, 1]   # c
+        ]
+        labels = ["a", "b", "c"]
+        weights = [1, 1, 1]
+        g = PartiallyDirectedGraph(full_adjacency_matrix_sample, labels, weights)
+        # g.plot()
+        g1 = G1().transform_from_partially_directed(g)
+        # TODO
 
 
 if __name__ == '__main__':
