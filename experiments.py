bc1e03fc7fd2e99e44dba770b7dbad6dead211cd
vonHousen <42911623+vonHousen@users.noreply.github.com>
2020-05-27 13:30:27 +0200

    Finished simple experiment.

--- experiments.py
+++ experiments.py
@@ -12,7 +12,7 @@ while not is_connected:
     is_connected = g.is_connected()
 
 # assign random weights to it
-weights = [random.randint(1, 10) for _ in range(vertex_count)]
+weights = [random.randint(10, 100) for _ in range(vertex_count)]
 vertex_labels = list(ascii_lowercase[:edge_count])
 g.es["weights"] = weights
 g.es["label"] = weights
@@ -26,7 +26,22 @@ for _ in range(undirected_count):   # set random edges as undirected
     edge_idx = random.randint(0, edge_count-1)
     full_adjacency_matrix[:, edge_idx] = -1*abs(full_adjacency_matrix[:, edge_idx])
 g = PartiallyDirectedGraph(full_adjacency_matrix, weights, vertex_labels)
-g.plotGraph()
+# g.plotGraph()
 g1 = G1(g)
 # g1.plotGraph()
-g1.get_postman_tour(100, 'a')
+cost, tour, tour_by_id = g1.get_postman_tour(1, 'a')
+
+# check the tour
+# iterate through each edge that tour consists of
+prev_vertex = None
+for vertex in tour_by_id:
+    if prev_vertex is not None:
+        edge_idx, is_upstream = g.get_edge_between(prev_vertex, vertex)
+        if is_upstream:
+            vertex_name = g.graph.vs[vertex]["label"]
+            prev_vertex_name = g.graph.vs[prev_vertex]["label"]
+            print(f"\n\nChinese postman tour consists of an upstream path (road: {prev_vertex_name}->{vertex_name})!")
+            g.plotGraph()
+            break
+
+    prev_vertex = vertex
